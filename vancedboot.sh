19a231c1dc7ef4a817ed7ebb4e6383a667a0ecd8
X1nto <burtkni@yandex.ru>
2020-02-04 18:18:42 +0400

    added backup for Recovery zip

---
+++ vancedboot.sh
@@ -0,0 +1,7 @@
+#!/sbin/sh
+
+ui_print ""
+ui_print "Backing up existing animation"
+mv /system/media/bootanimation.zip /system/media/bootanimation-backup.zip.bak
+ui_print "Backup succesful"
+ui_print ""
